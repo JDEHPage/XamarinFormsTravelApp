7e32b857c55191051e09f6f5fda07cfacc0bf1bc
John Daniel Page <jdehpage.w@gmail.com>
2020-10-14 13:46:16 +0100

    Implemented Icons
    - Implemented Icons on the home page and teacel page for both systems.

--- TravelRecordApp.Android/obj/Debug/android/environment.x86.s
+++ TravelRecordApp.Android/obj/Debug/android/environment.x86.s
@@ -71,7 +71,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"bbbec321-ba43-4672-9955-2a23c5e6cca6"
+	.asciz	"8f912048-a25b-46c3-aa50-45ce597307e6"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
