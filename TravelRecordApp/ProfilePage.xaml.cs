be03c2e17826a31c1dd1c437bf629882ff4df325
John Daniel Page <jdehpage.w@gmail.com>
2020-10-15 14:43:09 +0100

    Added categories to ProfilePage
    - Added categories to ProfilePage via Linq

--- TravelRecordApp/ProfilePage.xaml.cs
+++ TravelRecordApp/ProfilePage.xaml.cs
@@ -22,8 +22,31 @@ namespace TravelRecordApp
             {
                 var postTable = conn.Table<Post>().ToList();
 
+                //var categories = (from p in postTable
+                //                  orderby p.CategoryId
+                //                  select p.CategoryName).Distinct().ToList();
+
+                //This is the same as the first Linq categories query just shorter syntax
+                var categories2 = postTable.OrderBy(p => p.CategoryId).Select(p => p.CategoryName).Distinct().ToList();
+
+                Dictionary<string, int> categoryCount = new Dictionary<string, int>();
+
+                foreach(var category in categories2)
+                {
+                    //var count = (from post in postTable
+                    //             where post.CategoryName == category
+                    //             select post).ToList().Count;
+
+                    //This is the same as the first count Linq query just shorter syntax
+                    var count2 = postTable.Where(p => p.CategoryName == category).ToList().Count;
+
+                    categoryCount.Add(category, count2);
+                }
+
+                categoryListView.ItemsSource = categoryCount;
+
                 postCountLable.Text = postTable.Count.ToString();
             }
-        }
+        } 
     }
 }
